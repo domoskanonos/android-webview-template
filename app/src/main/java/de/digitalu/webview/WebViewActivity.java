4a5c37865cf062dfde4df409f50cdca3cfa8e0ef
u150239 <dominik.bruhn@signal-iduna.de>
2022-10-24 13:37:08 +0200

    init

---
+++ app/src/main/java/de/digitalu/webview/WebViewActivity.java
@@ -0,0 +1,84 @@
+package de.digital.u.taxlancer.webview;
+
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowInsets;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import de.digital.u.taxlancer.webview.databinding.ActivityFullscreenBinding;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class WebViewActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        de.digital.u.taxlancer.webview.databinding.ActivityFullscreenBinding binding = ActivityFullscreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        webView = binding.webView;
+
+        WebViewClientImpl webViewClient = new WebViewClientImpl(this);
+        webView.setWebViewClient(webViewClient);
+
+
+        if (Build.VERSION.SDK_INT >= 30) {
+            webView.getWindowInsetsController().hide(
+                    WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+        } else {
+            // Note that some of these constants are new as of API 16 (Jelly Bean)
+            // and API 19 (KitKat). It is safe to use them, as they are inlined
+            // at compile-time and do nothing on earlier devices.
+            webView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
+
+        //webView.setInitialScale(1);
+        //webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        //webView.setScrollbarFadingEnabled(false);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        //settings.setDefaultTextEncodingName("utf-8");
+        //settings.setLoadsImagesAutomatically(true);
+        settings.setAllowContentAccess(true);
+        //settings.setUseWideViewPort(true);
+        //settings.setLoadWithOverviewMode(true);
+
+        webView.loadUrl(WebViewClientImpl.DOMAIN);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && this.webView.canGoBack()) {
+            this.webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+}
---
+++ app/src/main/java/de/digitalu/webview/WebViewClientImpl.java
@@ -0,0 +1,34 @@
+package de.digital.u.taxlancer.webview;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewClientImpl extends WebViewClient {
+
+    public static final String DOMAIN = "https://www.taxlancer.de/";
+
+    private Activity activity;
+
+    public WebViewClientImpl(Activity activity) {
+        this.activity = activity;
+    }
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+
+        if (url.contains(DOMAIN)) return false;
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        activity.startActivity(intent);
+
+        return true;
+
+    }
+
+
+
+
+}
